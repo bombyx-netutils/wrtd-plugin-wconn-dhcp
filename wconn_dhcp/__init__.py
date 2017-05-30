d95a60ace5c421d33827c251329aa7b929133c75
fpemud <fpemud@sina.com>
2017-05-30 09:17:31 +0000

    fix

--- wconn_dhcp/__init__.py
+++ wconn_dhcp/__init__.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import os
-import fcntl
+import time
 import socket
-import struct
 import logging
 import pyroute2
 import netifaces
-from dhcpc import DhcpClient
+import subprocess
 
 
 def get_plugin_list():
@@ -29,22 +28,21 @@ class _PluginObject:
     def __init__(self):
         pass
 
-    def init2(self, cfg, tmpDir, ownResolvConf, prefixCheckFunc):
+    def init2(self, cfg, tmpDir, ownResolvConf):
         self.cfg = cfg
         self.tmpDir = tmpDir
         self.ownResolvConf = ownResolvConf
-        self.prefixCheckFunc = prefixCheckFunc
-
-        self.logger = logging.getLogger(__name__ + ".generic-dhcp")
-        self.dhClient = None
+        self.logger = logging.getLogger(self.__module__ + "." + self.__class__.__name__ + ".generic-dhcp")
+        self.proc = None
 
     def start(self):
         self.logger.info("Started.")
 
     def stop(self):
-        if self.dhClient is not None:
-            self.dhClient.stop()
-            self.dhClient = None
+        if self.proc is not None:
+            self.proc.terminate()
+            self.proc.join()
+            self.proc = None
             with pyroute2.IPRoute() as ip:
                 idx = ip.link_lookup(ifname=self.cfg["interface"])[0]
                 ip.link("set", index=idx, state="down")
@@ -58,55 +56,55 @@ class _PluginObject:
         if ifname != self.cfg["interface"]:
             return False
 
+        assert self.proc is None
+
         with pyroute2.IPRoute() as ip:
             idx = ip.link_lookup(ifname=self.cfg["interface"])[0]
             ip.link("set", index=idx, state="up")
 
-        assert self.dhClient is None
-        self.dhClient = DhcpClient(self.cfg["interface"], _BackEnd(self))
-        self.dhClient.start()
+        # create dhclient.conf, copied from nm-dhcp-dhclient-utils.c in networkmanager-1.4.4
+        cfgf = os.path.join(self.tmpDir, "dhclient.conf")
+        with open(cfgf, "w") as f:
+            buf = ""
+            buf += "send host-name \"%s\";\n" % (socket.gethostname())
+            buf += "\n"
+            buf += "option rfc3442-classless-static-routes code 121 = array of unsigned integer 8;\n"
+            buf += "option wpad code 252 = string;\n"
+            buf += "\n"
+            buf += "also request rfc3442-classless-static-routes;\n"
+            buf += "also request static-routes;\n"
+            buf += "also request wpad;\n"
+            buf += "also request ntp-servers;\n"
+            f.write(buf)
+
+        self.proc = subprocess.Popen([
+            "/usr/bin/python3",
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), "subproc_dhclient.py"),
+            self.tmpDir,
+            cfgf,
+            self.cfg["interface"],
+            self.ownResolvConf,
+        ])
+
+        # wait for ip address
+        i = 0
+        while True:
+            t = netifaces.ifaddresses(self.cfg["interface"])
+            if 2 not in t:
+                if i >= 10:
+                    raise Exception("IP address allocation time out.")
+                time.sleep(1.0)
+                i += 1
+                continue
+            break
 
         self.logger.info("Interface \"%s\" managed." % (self.cfg["interface"]))
         return True
 
     def interface_disappear(self, ifname):
         assert ifname == self.cfg["interface"]
-        if self.dhClient is not None:
-            self.dhClient.stop()
-            self.dhClient = None
+        if self.proc is not None:
+            self.proc.terminate()
+            self.proc.wait()
+            self.proc = None
         self.logger.info("Interface \"%s\" unmanaged." % (self.cfg["interface"]))
-
-
-class _BackEnd:
-
-    def __init__(self, pObj):
-        self.pObj = pObj
-        self.addr = None
-
-    def get_saved_lease(self):
-        return None
-
-    def lease_acquired(self, ifname, lease):
-        # delete old address
-        if self.addr is not None:
-            with pyroute2.IPRoute() as ip:
-                idx = ip.link_lookup(ifname=ifname)[0]
-                ip.addr("delete", index=idx, address=self.addr)
-            self.addr = None
-
-        # set new address, default route and dns information
-        with pyroute2.IPRoute() as ipp:
-            idx = ipp.link_lookup(ifname=ifname)[0]
-            ipp.addr("add", index=idx, address=lease.address, mask=lease.subnet_mask, broadcast=lease.broadcast)
-            ipp.route("add", dst="0.0.0.0/0", gateway=lease.router[0], oif=idx)
-        self.addr = lease.address
-
-    def lease_updated(self, ifname, lease):
-        self.lease_acquired(ifname, lease)
-
-    def lease_destroyed(self, ifname):
-        if self.addr is not None:
-            with pyroute2.IPRoute() as ip:
-                idx = ip.link_lookup(ifname=ifname)[0]
-                ip.addr("delete", index=idx, address=self.addr)
-            self.addr = None
---
+++ wconn_dhcp/subproc_dhclient.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+import os
+import sys
+import shutil
+import ctypes
+import errno
+import subprocess
+
+
+class _UtilNewMountNamespace:
+
+    _CLONE_NEWNS = 0x00020000               # <linux/sched.h>
+    _MS_REC = 16384                         # <sys/mount.h>
+    _MS_PRIVATE = 1 << 18                   # <sys/mount.h>
+    _libc = None
+    _mount = None
+    _setns = None
+    _unshare = None
+
+    def __init__(self):
+        if self._libc is None:
+            self._libc = ctypes.CDLL('libc.so.6', use_errno=True)
+            self._mount = self._libc.mount
+            self._mount.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p]
+            self._mount.restype = ctypes.c_int
+            self._setns = self._libc.setns
+            self._unshare = self._libc.unshare
+
+        self.parentfd = None
+
+    def __enter__(self):
+        self.parentfd = open("/proc/%d/ns/mnt" % (os.getpid()), 'r')
+
+        # copied from unshare.c of util-linux
+        try:
+            if self._unshare(self._CLONE_NEWNS) != 0:
+                e = ctypes.get_errno()
+                raise OSError(e, errno.errorcode[e])
+
+            srcdir = ctypes.c_char_p("none".encode("utf_8"))
+            target = ctypes.c_char_p("/".encode("utf_8"))
+            if self._mount(srcdir, target, None, (self._MS_REC | self._MS_PRIVATE), None) != 0:
+                e = ctypes.get_errno()
+                raise OSError(e, errno.errorcode[e])
+        except BaseException:
+            self.parentfd.close()
+            self.parentfd = None
+            raise
+
+    def __exit__(self, *_):
+        self._setns(self.parentfd.fileno(), 0)
+        self.parentfd.close()
+        self.parentfd = None
+
+
+assert len(sys.argv) == 4
+tmpDir = sys.argv[1]
+cfgf = sys.argv[2]
+intfName = sys.argv[3]
+ownResolvConf = sys.argv[4]
+
+selfDir = os.path.dirname(os.path.realpath(__file__))
+tmpEtcDhcpDir = os.path.join(tmpDir, "etc-dhcp")
+tmpEnterHook = os.path.join(tmpEtcDhcpDir, "dhclient-enter-hooks")
+tmpExitHook = os.path.join(tmpEtcDhcpDir, "dhclient-exit-hooks")
+proc = None
+
+try:
+    os.mkdir(tmpEtcDhcpDir)
+
+    buf = ""
+    with open(os.path.join(selfDir, "dhclient-enter-hooks"), "r") as f:
+        buf = f.read()
+    with open(os.path.join(tmpEnterHook), "w") as f:
+        buf = buf.replace("%%RESOLV_CONF%%", ownResolvConf)
+        f.write(buf)
+    os.chmod(tmpEnterHook, 0o755)
+
+    shutil.copy(os.path.join(selfDir, "dhclient-exit-hooks"), tmpExitHook)
+    os.chmod(tmpExitHook, 0o755)
+
+    with _UtilNewMountNamespace():
+        # dhclient read custom scripts from the fixed location /etc/dhcp
+        # this behavior sucks so we use mount namespace to workaround it
+        subprocess.check_call(["/bin/mount", "--bind", tmpEtcDhcpDir, "/etc/dhcp"])
+
+        cmd = "/sbin/dhclient "
+        cmd += "-d --no-pid "
+        cmd += "-cf %s " % (cfgf)
+        cmd += "-lf %s " % (os.path.join(tmpDir, "dhclient.leases"))
+        cmd += "%s >/dev/null 2>&1" % (intfName)
+
+        proc = subprocess.Popen(cmd, shell=True, universal_newlines=True)
+        proc.wait()
+except KeyboardInterrupt:
+    if proc is not None and not proc.poll():
+        proc.terminate()
+        proc.wait()
+finally:
+    shutil.rmtree(tmpEtcDhcpDir)
